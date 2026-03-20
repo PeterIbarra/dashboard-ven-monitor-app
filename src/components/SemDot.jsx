ecdd33de8dc8c9e26b39a7a9eb4934765c48dd7e
PeterIbarra <peter.ibarra@undp.org>
2026-03-20 15:53:13 -0400

    Delete src directory

--- src/components/SemDot.jsx
+++
@@ -1,8 +0,0 @@
-import { SEM } from "../constants";
-
-export function SemDot({ color, size = 8 }) {
-  return (
-    <span style={{ display:"inline-block", width:size, height:size,
-      borderRadius:"50%", background:SEM[color] || color, flexShrink:0 }} />
-  );
-}
